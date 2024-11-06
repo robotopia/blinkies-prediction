182db6decbd813312e9fc1a444a02f915e6da1f2
Natasha Hurley-Walker <nhw@icrar.org>
2024-11-06 13:33:02 +0800

    Fixed T0 to be middle of eclipse not middle of 'on'

--- J1723-33/J1723-33_predict.py
+++ J1723-33/J1723-33_predict.py
@@ -37,7 +37,6 @@ output_timezone = TimezoneInfo(utc_offset=5.5*u.hour)
 # Set to None or 0.0 for no offset
 offset = -0.5 * u.hour
 
-#T0 = Time(59805.48950, format='mjd', scale='utc', location=gmrt_loc)# start of eclipse
 T0 = Time(59789.56601, format='mjd', scale='utc', location=gmrt_loc)# middle of eclipse
 
 # Measurements from previous phase dispersion minimisation applied to MWA detections
@@ -56,11 +55,11 @@ duration = 0.15*Pb
 # How long between 50% on?
 off_time = Pb*(1 - duty_cycle)
 
-# Noting that middle of ingress is actually earlier than Flora's T0 by about 0.1 orbital phase
-# So middle of ingress is current T0 plus half of the duty_cycle; beginning is minus half of the duration
-ingress_epoch = T0 + 0.5*duty_cycle*Pb - 0.5*duration
-# And middle of egress is current T0 minus half of the duty_cyle; beginning is minus half of the duration
-egress_epoch = T0 - 0.5*duty_cycle*Pb - 0.5*duration
+# T0 is the middle of the eclipse
+# So middle of ingress is current T0 minus half of 1-duty_cycle; beginning is minus half of the duration
+ingress_epoch = T0 - 0.5*(1-duty_cycle)*Pb - 0.5*duration
+# And middle of egress is current T0 plus half of 1-duty_cyle; beginning is minus half of the duration
+egress_epoch = T0 + 0.5*(1-duty_cycle)*Pb - 0.5*duration
 
 # Calculate the number of orbital periods since the assumed egress/ingress epochs that fall within the given MJD range
 egress_rotation_start = int(np.ceil((MJD_start - egress_epoch)/Pb))
@@ -83,6 +82,9 @@ if offset is not None:
 #for egress in egresses:
 #    print(egress.to_datetime(timezone=output_timezone))
 
+# How many minutes you are happy to start away from integer IST scheduling blocks
+flex = 20
+
 # Convert to specified timezone and print out
 print(f"Egresses {offset.to('hour'):+f}:")
 for egress in egresses:
@@ -90,15 +92,15 @@ for egress in egresses:
 # Will have to relax the timing constraint and then the offset because we're not finding anything
         t = egress.to_datetime(timezone=output_timezone)
         minutes = t.minute
-        if minutes > 30 or minutes < 30:
+        if minutes > 60-flex or minutes < flex:
     # Also needs to be observable "duration" hours later
             starttime = egress
             endtime = egress + duration.to(u.hour)
             t = starttime.to_datetime(timezone=output_timezone)
             if is_observable(ac, gmrt, J1723, starttime) and is_observable(ac, gmrt, J1723, endtime):
-                 if minutes > 30:
+                 if minutes > 60-flex:
                      hr = t.hour + 1
-                 elif minutes < 30:
+                 elif minutes < flex:
                      hr = t.hour
                  print("IST: {0}; LST: {1:2.3f}; Scheduling block: {2:4d}-{3:02d}-{4:02d} {5:02d}:00:00".format(t, starttime.sidereal_time('apparent'), t.year, t.month, t.day, hr))
 
@@ -108,15 +110,15 @@ for ingress in ingresses:
 # Needs to start very close to (within ten minutes of) a whole number of hours in IST
         t = ingress.to_datetime(timezone=output_timezone)
         minutes = t.minute
-        if minutes > 30 or minutes < 30:
+        if minutes > 60-flex or minutes < flex:
     # Also needs to be observable "duration" hours later
             starttime = ingress
             endtime = ingress + duration.to(u.hour)
             t = starttime.to_datetime(timezone=output_timezone)
             if is_observable(ac, gmrt, J1723, starttime) and is_observable(ac, gmrt, J1723, endtime):
-                 if minutes > 30:
+                 if minutes > 60-flex:
                      hr = t.hour + 1
-                 elif minutes < 30:
+                 elif minutes < flex:
                      hr = t.hour
                  print("IST: {0}; LST: {1:2.3f}; Scheduling block: {2:4d}-{3:02d}-{4:02d} {5:02d}:00:00".format(t, starttime.sidereal_time('apparent'), t.year, t.month, t.day, hr))
 
