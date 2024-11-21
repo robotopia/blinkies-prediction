db7b2f8248650e600ef632c0af8a887170523682
Natasha Hurley-Walker <nhw@icrar.org>
2024-11-21 11:50:12 +0800

    Attempts to schedule least-observed source; updated ephemerides

--- MeerKAT_observing/find_best_dates.py
+++ MeerKAT_observing/find_best_dates.py
@@ -27,24 +27,23 @@ coords = {"J1646": FixedTarget(SkyCoord("16 46 22.7", "-44 05 41", frame="fk5", 
           "J1815" : FixedTarget(SkyCoord("18 15 56.8", "-14 16 34", frame="fk5", unit=(u.hour, u.deg)), name="J1815")}
 
 # T0 = middle of eclipse, in MJD
-# THESE ARE PLACEHOLDERS UNTIL CONFIRMED BY FLORA
-T0s = {"J1646" : Time(59700, format='mjd', scale='utc', location=mkt.location),
-       "J1723" : Time(59788.54, format='mjd', scale='utc', location=mkt.location),
-       "J1728" : Time(59700, format='mjd', scale='utc', location=mkt.location),
-       "J1734" : Time(59765.611, format='mjd', scale='utc', location=mkt.location),
-       "J1740" : Time(59700, format='mjd', scale='utc', location=mkt.location),
-       "J1752" : Time(59791.54, format='mjd', scale='utc', location=mkt.location),
-       "J1815" : Time(59732.73, format='mjd', scale='utc', location=mkt.location)}
+# I have scaled J1646's T0 to 0.26 of orbital phase based on Fig 3
+T0s = {"J1646" : Time(59391.3567+0.26*(5.26703./24.), format='mjd', scale='utc', location=mkt.location),
+       "J1723" : Time(59789.566010035276, format='mjd', scale='utc', location=mkt.location),
+       "J1728" : Time(60055.87090177478, format='mjd', scale='utc', location=mkt.location),
+       "J1734" : Time(59796.055897211045, format='mjd', scale='utc', location=mkt.location),
+       "J1740" : Time(60056.868761778795, format='mjd', scale='utc', location=mkt.location),
+       "J1752" : Time(59807.08632836161, format='mjd', scale='utc', location=mkt.location),
+       "J1815" : Time(59740.04800876778, format='mjd', scale='utc', location=mkt.location)}
 
 # Orbital periods
-# GOOD FOR FLORA TO CHECK THESE
 Pbs = {"J1646" : 5.26703*u.hour,
-       "J1723" : 22.6824*u.hour,
+       "J1723" : 22.68495*u.hour,
        "J1728" : 5.04983*u.hour,
-       "J1734" : 10.93128*u.hour,
+       "J1734" : 10.92946*u.hour,
        "J1740" : 16.50614*u.hour,
-       "J1752" : 17.8632*u.hour,
-       "J1815" : 9.82272*u.hour}
+       "J1752" : 17.85847*u.hour,
+       "J1815" : 9.81969*u.hour}
 
 # duty cycle of ON phase
 duty_cycles = {"J1646" : 0.5,
@@ -68,25 +67,17 @@ constraints = {}
 for obj in T0s:
     constraints[obj] = [PhaseConstraint(pe[obj], min=0.5 - (duty_cycles[obj]/2), max=0.5 + (duty_cycles[obj]/2)), ac]
 
-# Number of (15-min) observations taken so far (zero for all objects)
-observed = {"J1646" : 0,
-       "J1723" : 0,
-       "J1728" : 0,
-       "J1734" : 0,
-       "J1740" : 0,
-       "J1752" : 0,
-       "J1815" : 0}
-
 #-------------------------------------------------#
 # Input parameters and settings (user may change) #
 #-------------------------------------------------#
 
 
 # Change the range of desired MJD prediction here
-MJD_start = Time("2024-11-15T00:00:00", format='isot', scale="utc", location=mkt.location)
-#MJD_stop = Time("2025-11-15T00:00:00", format='isot', scale="utc", location=mkt.location)
+MJD_start = Time("2024-11-30T00:00:00", format='isot', scale="utc", location=mkt.location)
+MJD_stop = Time("2025-11-15T00:00:00", format='isot', scale="utc", location=mkt.location)
 # For debugging
-MJD_stop = Time("2024-11-16T00:00:00", format='isot', scale="utc", location=mkt.location)
+#MJD_stop = Time("2024-12-15T00:00:00", format='isot', scale="utc", location=mkt.location)
+
 
 #---------------------------------------#
 # Calculations (user should not change) #
@@ -105,114 +96,39 @@ for mjd in np.arange(MJD_start.mjd, MJD_stop.mjd):
 
 # Find out when the start and stop times are for a given day
 
+obslength = []
 for mjd in ok:
     start = False
     stop = False
     for hour in range(0, 24):
         observability = [is_observable(constraints[obj], mkt, coords[obj], time_range=[Time(mjd+(hour/24), format='mjd', scale='utc'), Time(mjd+((hour+1)/24.), format='mjd', scale='utc')])[0] for obj in T0s]
-        print(hour, observability, np.any(observability))
         if not start and np.any(observability):
              start = hour
         if start and not stop and not np.any(observability):
              stop = hour
     print(f"For {mjd}, observability starts at {start} and ends at {stop}")
+    if stop - start > 8:
     
-# Create a detailed schedule by observing each source for 15 minutes and moving on to the next source that is available
-# TODO: change this to the source that has the most change in orbital phase?
-    schedule = []
-    for hour in np.arange(start, stop, 0.25):
-         for obj in T0s:
-              if is_observable(constraints[obj], mkt, coords[obj], time_range=[Time(mjd+(hour/24), format='mjd', scale='utc'), Time(mjd+((hour+0.25)/24.), format='mjd', scale='utc')]) and observed[obj] <= 4:
-                   schedule.append(obj)
-                   observed[obj] += 1
-                   
+# Create a detailed schedule by observing each source for 15 minutes and moving on to the next source with the fewest observations so far
+        schedule = []
+        maxobs = 1
+        minobs = 0
+# Number of (15-min) observations taken so far (zero for all objects)
+        observed = {"J1646" : 0,
+               "J1723" : 0,
+               "J1728" : 0,
+               "J1734" : 0,
+               "J1740" : 0,
+               "J1752" : 0,
+               "J1815" : 0}
 
-print(schedule)
-                    
-        
+        for hour in np.arange(start, stop, 0.25):
+             for obj in T0s:
+                  if is_observable(constraints[obj], mkt, coords[obj], time_range=[Time(mjd+(hour/24), format='mjd', scale='utc'), Time(mjd+((hour+0.25)/24.), format='mjd', scale='utc')]) and observed[obj] <= 4 and observed[obj] <= minobs:
+                       schedule.append(obj)
+                       observed[obj] += 1
+                       minobs = min(observed.values())
 
-
-# If that doesn't work, re-sort the sources and try again..?
-
-
-
-# Of the days which were possible, check that the observing time will be used efficiently -- that at any given point there is always a source to measure
-# Output timezone = UTC
-#output_timezone = TimezoneInfo(utc_offset=0*u.hour)
-#
-## Offset reported times by some custom amount (e.g. half an hour before the ingress/egress times)?
-## Set to None or 0.0 for no offset
-#offset = 0.0 * u.hour
-#
-#
-## How long between 50% on?
-#off_time = Pb*(1 - duty_cycle)
-#
-## T0 is the middle of the eclipse
-#
-## So middle of ingress is current T0 minus half of 1-duty_cycle; beginning is minus half of the duration
-#ingress_epoch = T0 - 0.5*(1-duty_cycle)*Pb - 0.5*duration
-## And middle of egress is current T0 plus half of 1-duty_cyle; beginning is minus half of the duration
-#egress_epoch = T0 + 0.5*(1-duty_cycle)*Pb - 0.5*duration
-#
-## Calculate the number of orbital periods since the assumed egress/ingress epochs that fall within the given MJD range
-#egress_rotation_start = int(np.ceil((MJD_start - egress_epoch)/Pb))
-#egress_rotation_stop = int(np.ceil((MJD_stop - egress_epoch)/Pb))
-#ingress_rotation_start = int(np.ceil((MJD_start - ingress_epoch)/Pb))
-#ingress_rotation_stop = int(np.ceil((MJD_stop - ingress_epoch)/Pb))
-#
-## Calculate the actual ingress/egress times
-#egresses = np.arange(egress_rotation_start, egress_rotation_stop)*Pb + egress_epoch
-#ingresses = np.arange(ingress_rotation_start, ingress_rotation_stop)*Pb + ingress_epoch
-#
-## Apply the offset (to allow time for phase calibration etc)
-#if offset is not None:
-#    egresses += offset
-#    ingresses += offset
-#
-## For this source we only want to try to get some "on" measurements
-## Convert to specified timezone and print out
-##print(f"Egresses {offset.to('hour'):+f}:")
-##for egress in egresses:
-##    print(egress.to_datetime(timezone=output_timezone))
-#
-## How many minutes you are happy to start away from integer IST scheduling blocks
-#flex = 20
-#
-## Convert to specified timezone and print out
-#print(f"Egresses {offset.to('hour'):+f}:")
-#for egress in egresses:
-#    if is_observable(ac, gmrt, J1723, egress):
-## Will have to relax the timing constraint and then the offset because we're not finding anything
-#        t = egress.to_datetime(timezone=output_timezone)
-#        minutes = t.minute
-#        if minutes > 60-flex or minutes < flex:
-#    # Also needs to be observable "duration" hours later
-#            starttime = egress
-#            endtime = egress + duration.to(u.hour)
-#            t = starttime.to_datetime(timezone=output_timezone)
-#            if is_observable(ac, gmrt, J1723, starttime) and is_observable(ac, gmrt, J1723, endtime):
-#                 if minutes > 60-flex:
-#                     hr = t.hour + 1
-#                 elif minutes < flex:
-#                     hr = t.hour
-#                 print("IST: {0}; LST: {1:2.3f}; Scheduling block: {2:4d}-{3:02d}-{4:02d} {5:02d}:00:00".format(t, starttime.sidereal_time('apparent'), t.year, t.month, t.day, hr))
-#
-#print(f"Ingresses {offset.to('hour'):+f}:")
-#for ingress in ingresses:
-#    if is_observable(ac, gmrt, J1723, ingress):
-## Needs to start very close to (within ten minutes of) a whole number of hours in IST
-#        t = ingress.to_datetime(timezone=output_timezone)
-#        minutes = t.minute
-#        if minutes > 60-flex or minutes < flex:
-#    # Also needs to be observable "duration" hours later
-#            starttime = ingress
-#            endtime = ingress + duration.to(u.hour)
-#            t = starttime.to_datetime(timezone=output_timezone)
-#            if is_observable(ac, gmrt, J1723, starttime) and is_observable(ac, gmrt, J1723, endtime):
-#                 if minutes > 60-flex:
-#                     hr = t.hour + 1
-#                 elif minutes < flex:
-#                     hr = t.hour
-#                 print("IST: {0}; LST: {1:2.3f}; Scheduling block: {2:4d}-{3:02d}-{4:02d} {5:02d}:00:00".format(t, starttime.sidereal_time('apparent'), t.year, t.month, t.day, hr))
-
+        if len(schedule) > 25:
+            print(f"{mjd} is an ideal day, with a schedule that starts at {start}, ends at {stop}, and iterates through the sources in the following order:")
+            print(schedule)
